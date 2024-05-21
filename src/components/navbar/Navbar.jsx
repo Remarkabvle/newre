0af85673cef9b85aaa6c6b22be69efc15dd0b6f6
Abubakr <abubakrdev69@gmail.com>
2024-05-21 23:18:46 +0500

    add all pages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import './navbar.css';
+import image from '../../assets/image.png'
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar">
-        <div className="navbar-brand">
-          <a href="/">Brand</a>
+      <section className="header">
+        <nav className="navbar">
+          <div className="navbar-brand">
+            <a href="/">AliExpress</a>
+          </div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="/">каталог</a>
+            </li>
+            <li>
+              <input type="text" placeholder='серьги ' />
+            </li>
+            <li className="nav-item">
+              <a href="/about">заказы </a>
+            </li>
+            <li className="nav-item">
+              <a href="/services">корзина</a>
+            </li>
+            <li className="nav-item">
+              <a href="/contact">войти </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="hero">
+            <img src={image} alt="" />
         </div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="/about">About</a>
-          </li>
-          <li className="nav-item">
-            <a href="/services">Services</a>
-          </li>
-          <li className="nav-item">
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      </section>
     );
   }
 }
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './Products.css';
+
 
 const API_URL = "https://dummyjson.com";
 
@@ -9,51 +10,105 @@ export default class Products extends Component {
         super();
         this.state = {
             data: null,
+            categories: null,
             offset: 1,
-            counter: 0
+            counter: 0,
+            categoryValue: "/products",
+            loading: false
         };
     }
 
     componentDidMount() {
-        axios
-            .get(`${API_URL}/products?limit=${this.state.offset * 2}`)
-            .then((res) => this.setState({ data: res.data.products }))
-            .catch((err) => console.log(err));
+        this.fetchProducts();
+        this.fetchCategories();
     }
 
-    componentDidUpdate(prevProvs, prevState) {
-        if (this.state.offset !== prevState.offset) {
-            axios
-                .get(`${API_URL}/products?limit=${this.state.offset * 2}`)
-                .then((res) => this.setState({ data: res.data.products }))
-                .catch((err) => console.log(err));
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.offset !== prevState.offset || this.state.categoryValue !== prevState.categoryValue) {
+            this.fetchProducts();
+            if (this.state.categoryValue !== prevState.categoryValue) {
+                this.setState({ offset: 1 });
+            }
         }
-        if (this.state.counter !== prevState.counter) {
-            console.log("counter render");
-        }
+    }
+
+    fetchProducts() {
+        this.setState({ loading: true });
+        axios
+            .get(`${API_URL}${this.state.categoryValue}?limit=${this.state.offset * 12}`)
+            .then(res => this.setState({ data: res.data.products, loading: false }))
+            .catch(err => console.log(err));
+    }
+
+    fetchCategories() {
+        axios
+            .get(`${API_URL}/products/categories`)
+            .then(res => this.setState({ categories: res.data }))
+            .catch(err => console.error(err));
     }
 
     render() {
+        const categories = this.state.categories?.map((category) => (
+            <li key={category}>
+                <button
+                    style={{
+                        color: this.state.categoryValue === `/products/category/${category}` ? "#000" : "#A0A0AB",
+                        borderBottom: this.state.categoryValue === `/products/category/${category}` ? "2px solid #000" : "2px solid transparent",
+                        background: "none",
+                        border: "none",
+                        cursor: "pointer"
+                    }}
+                    onClick={() => this.setState({ categoryValue: `/products/category/${category}`, offset: 1 })}
+                >
+                    {category.charAt(0).toUpperCase() + category.slice(1).toLowerCase()}
+                </button>
+            </li>
+        ));
+
         return (
-            <section>
-                <h2 className="header">Products</h2>
-                <div className="container">
-                    {
-                        this.state.data ? (
+            <Fragment>
+                <section className='products container'>
+                    <div className='products__categories'>
+                        <ul className='products__list'>
+                            <li>
+                                <button
+                                    style={{
+                                        color: this.state.categoryValue === `/products` ? "#000" : "#A0A0AB",
+                                        borderBottom: this.state.categoryValue === `/products` ? "2px solid #000" : "2px solid transparent",
+                                        background: "none",
+                                        border: "none",
+                                        cursor: "pointer"
+                                    }}
+                                    onClick={() => this.setState({ categoryValue: `/products`, offset: 1 })}
+                                >
+                                    All
+                                </button>
+                            </li>
+                            {categories}
+                        </ul>
+                    </div>
+                    <div className='products__cards'>
+                        {this.state.data ? (
                             this.state.data.map((el) => (
-                                    <div key={el.id} className="product">
-                                        <img src={el.images[0]} alt={el.title} className="product-image" />
-                                        <p className="product-title">{el.title}</p>
-                                        <p className="product-price">{el.price}$</p>
-                                    </div>
+                                <div key={el.id} className="product">
+                                    <img src={el.images[0]} alt={el.title} className="product-image" />
+                                    <p className="product-title">{el.title}</p>
+                                    <p className="product-price">{el.price}$</p>
+                                </div>
                             ))
                         ) : (
                             <p>Loading...</p>
-                        )
-                    }
-                </div>
-                    <button onClick={()=> this.setState({offset: this.state.offset +1})}>See more</button>
-            </section>
+                        )}
+                    </div>
+                    <button
+                        disabled={this.state.loading}
+                        onClick={() => this.setState({ offset: this.state.offset + 1 })}
+                        className='products__btn'
+                    >
+                        показать ещё 
+                    </button>
+                </section>
+            </Fragment>
         );
     }
 }
